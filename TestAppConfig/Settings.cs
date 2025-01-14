ad396998517129d21ab99d3e9d9ad85e4b91aa32
Florian Wagner <h2floh@h2floh.net>
2025-01-14 15:21:35 +0000

    Add initial Razor Pages and configuration for TestAppConfig

---
+++ TestAppConfig/Settings.cs
@@ -0,0 +1,10 @@
+namespace TestAppConfig
+{
+    public class Settings
+    {
+        public string BackgroundColor { get; set; } = "not loaded";
+        public long FontSize { get; set; } = 1;
+        public string FontColor { get; set; } = "not loaded";
+        public string Message { get; set; } = "not loaded";
+    }
+}
--- my-dotnet-core-app/src/Models/FeatureFlag.cs
+++
@@ -1,6 +0,0 @@
-public class FeatureFlag
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public bool IsEnabled { get; set; }
-}
